e4bc44abbea734f9ed772b136ece9ab8c09b7b36
Nuruddin Ashr <uudashr@gmail.com>
2020-05-07 16:31:52 +0700

    Use registered path instead of URL

--- middleware.go
+++ middleware.go
@@ -1,16 +1,16 @@
 package apm
 
 import (
+	"net/url"
+
 	"github.com/labstack/echo/v4"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
-	"net/http"
-	"net/url"
 )
 
 type mwOptions struct {
-	opNameFunc    func(r *http.Request) string
-	spanObserver  func(span opentracing.Span, r *http.Request)
+	opNameFunc    func(c echo.Context) string
+	spanObserver  func(span opentracing.Span, c echo.Context)
 	urlTagFunc    func(u *url.URL) string
 	componentName string
 }
@@ -19,15 +19,15 @@ func Middleware(componentName string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 
-			r := c.Request();
+			r := c.Request()
 			tracer := opentracing.GlobalTracer()
 
 			opts := mwOptions{
 				componentName: componentName,
-				opNameFunc: func(r *http.Request) string {
-					return "HTTP " + r.Method + " " + r.URL.Path
+				opNameFunc: func(ec echo.Context) string {
+					return "HTTP " + ec.Request().Method + " " + ec.Path()
 				},
-				spanObserver: func(span opentracing.Span, r *http.Request) {
+				spanObserver: func(span opentracing.Span, c echo.Context) {
 
 				},
 				urlTagFunc: func(u *url.URL) string {
@@ -37,13 +37,13 @@ func Middleware(componentName string) echo.MiddlewareFunc {
 
 			carrier := opentracing.HTTPHeadersCarrier(r.Header)
 			ctx, _ := tracer.Extract(opentracing.HTTPHeaders, carrier)
-			op := opts.opNameFunc(r)
+			op := opts.opNameFunc(c)
 			sp := opentracing.StartSpan(op, ext.RPCServerOption(ctx))
 			defer sp.Finish()
 
 			ext.HTTPMethod.Set(sp, r.Method)
 			ext.HTTPUrl.Set(sp, opts.urlTagFunc(r.URL))
-			opts.spanObserver(sp, r)
+			opts.spanObserver(sp, c)
 			ext.Component.Set(sp, opts.componentName)
 
 			r = r.WithContext(opentracing.ContextWithSpan(r.Context(), sp))
